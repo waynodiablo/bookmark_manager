730ff875e710d66be260a4bd98131cc7103c112c
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-11-20 16:03:52 +0000

    tests corrected to pass

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -5,7 +5,7 @@ describe Bookmark do
     it 'returns all bookmarks' do
       bookmarks = Bookmark.all
       expect(bookmarks).to include("http://www.google.com")
-      expect(bookmarks).to include("www.makersacademy.com")
+      expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
     end
   end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -7,7 +7,7 @@ feature "viewing bookmarks" do
   scenario 'visiting the Bookmarks list page' do
     visit ('/bookmarks')
     expect(page).to have_content "http://www.google.com"
-    expect(page).to have_content "www.makersacademy.com"
+    expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.destroyallsoftware.com"
   end
 end
